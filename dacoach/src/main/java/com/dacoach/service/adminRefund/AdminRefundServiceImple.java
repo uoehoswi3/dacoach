50d6d7fd3078279000fd07e0785ce37f8e3ecfaa
O1Jae <staregreg01@naver.com>
2026-03-18 21:54:13 +0900

    Update AdminRefundServiceImple.java

--- dacoach/src/main/java/com/dacoach/service/adminRefund/AdminRefundServiceImple.java
+++ dacoach/src/main/java/com/dacoach/service/adminRefund/AdminRefundServiceImple.java
@@ -30,38 +30,41 @@ public class AdminRefundServiceImple implements AdminRefundService{
 		}
 	}
 	@Transactional
-	@Override
-	public String approveRefund(int pay_idx) {
-		try {
-			PayDTO payInfo = kakakaoMapper.paySelect(pay_idx);
-			
-			if(payInfo == null) return "NOT_FOUND";
-			
-			Map<String,Object> parameters = new HashMap<>();
-			parameters.put("cid",payInfo.getCid());
-			parameters.put("tid", payInfo.getTid());
-			parameters.put("cancel_amount", payInfo.getTotal());
-			parameters.put("cancel_vat_amount", payInfo.getVat());
-			parameters.put("cancel_tax_free_amount", payInfo.getTax_free());
-			parameters.put("payload", payInfo.getPayload());
-			
-			KakaoCancelResponse response = kakaoPayService.cancelResponse(parameters);
-			if(response != null && "CANCEL_PAYMENT".equals(response.getStatus())) {
-				kakakaoMapper.cancelOk(response);
-				
-				Map<String,String> status_map=new HashMap<>();
-				status_map.put("status", "환불완료");
-				status_map.put("tid", payInfo.getTid());
-				kakakaoMapper.upPayStatus(status_map);
-				
-				return "SUCCESS";
-			}
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return "FAIL";
-	}
+@Override
+public String approveRefund(int pay_idx) {
+    try {
+        PayDTO payInfo = kakakaoMapper.paySelect(pay_idx);
+        
+        if(payInfo == null) return "NOT_FOUND";
+        
+        Map<String,Object> parameters = new HashMap<>();
+        parameters.put("cid", payInfo.getCid());
+        parameters.put("tid", payInfo.getTid());
+        parameters.put("cancel_amount", payInfo.getTotal());
+        parameters.put("cancel_vat_amount", payInfo.getVat());
+        parameters.put("cancel_tax_free_amount", payInfo.getTax_free());
+        parameters.put("payload", payInfo.getPayload());
+        
+        KakaoCancelResponse response = kakaoPayService.cancelResponse(parameters);
+        
+        if(response != null && "CANCEL_PAYMENT".equals(response.getStatus())) {
+            kakakaoMapper.cancelOk(response);
+            
+            Map<String,String> status_map = new HashMap<>();
+            status_map.put("status", "환불완료");
+            status_map.put("tid", payInfo.getTid());
+            kakakaoMapper.upPayStatus(status_map);
+            
+            return "SUCCESS";
+        }
+        
+        return "FAIL";
+        
+    } catch (Exception e) {
+        e.printStackTrace();
+        throw new RuntimeException("환불 처리 중 오류가 발생했습니다.", e);
+    }
+}
 
 	@Override
 	public int getRefundTotalCnt() {
