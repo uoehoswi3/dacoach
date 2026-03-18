416e3ae3c41a700ed1edfc9b464bd99ed734b36f
O1Jae <staregreg01@naver.com>
2026-03-18 21:54:42 +0900

    Update AdminRefundServiceImple.java

--- dacoach/src/main/java/com/dacoach/service/adminRefund/AdminRefundServiceImple.java
+++ dacoach/src/main/java/com/dacoach/service/adminRefund/AdminRefundServiceImple.java
@@ -29,7 +29,7 @@ public class AdminRefundServiceImple implements AdminRefundService{
 			return new ArrayList<>();
 		}
 	}
-	@Transactional
+	@Transactional(rollbackFor = Exception.class)
 @Override
 public String approveRefund(int pay_idx) {
     try {
